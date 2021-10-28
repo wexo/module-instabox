063750ac5da0843b5bf9e48fc3b2b266a09f0fa2
Anders Rasmussen <ar@wexo.dk>
2021-10-28 11:37:47 +0200

    :construction: [INSTA-9] copy parcelshop methodtype to instahome methodtype

---
+++ Model/MethodType/Instahome.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace Wexo\Instabox\Model\MethodType;
+
+use Magento\Framework\Api\DataObjectHelper;
+use Magento\Framework\Data\ObjectFactory;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Serialize\Serializer\Json;
+use Magento\Quote\Api\Data\CartInterface;
+use Magento\Sales\Api\Data\OrderAddressInterface;
+use Magento\Sales\Api\Data\OrderInterface;
+use Wexo\Instabox\Model\Api;
+use Wexo\Shipping\Api\Carrier\MethodTypeHandlerInterface;
+use Wexo\Shipping\Model\MethodType\AbstractParcelShop;
+
+class Instahome extends AbstractParcelShop implements MethodTypeHandlerInterface
+{
+    /**
+     * @var Json
+     */
+    private $jsonSerializer;
+    /**
+     * @var DataObjectHelper
+     */
+    private $dataObjectHelper;
+    /**
+     * @var ObjectFactory
+     */
+    private $objectFactory;
+    /**
+     * @var null
+     */
+    private $parcelShopClass;
+    /**
+     * @var Api
+     */
+    private $api;
+
+    public function __construct(
+        Json $jsonSerializer,
+        DataObjectHelper $dataObjectHelper,
+        ObjectFactory $objectFactory,
+        Api $api,
+        $parcelShopClass = null
+    ) {
+        parent::__construct(
+            $jsonSerializer,
+            $dataObjectHelper,
+            $objectFactory,
+            $parcelShopClass
+        );
+        $this->jsonSerializer = $jsonSerializer;
+        $this->dataObjectHelper = $dataObjectHelper;
+        $this->objectFactory = $objectFactory;
+        $this->parcelShopClass = $parcelShopClass;
+        $this->api = $api;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return __('Instahome');
+    }
+
+    /**
+     * @return string
+     */
+    public function getCode(): string
+    {
+        return 'instahome';
+    }
+
+    /**
+     * @param CartInterface $quote
+     * @param OrderInterface $order
+     * @throws LocalizedException
+     */
+    // TODO: Change this to do it correctly with instahome
+    public function saveOrderInformation(CartInterface $quote, OrderInterface $order)
+    {
+        $shippingData = $this->jsonSerializer->unserialize($order->getData('wexo_shipping_data'));
+
+        if (!isset($shippingData['parcelShop'])) {
+            throw new LocalizedException(__('Service Point must be set!'));
+        }
+
+        /** @var ParcelShopInterface $parcelShop */
+        $parcelShop = $this->objectFactory->create($this->parcelShopClass, []);
+        $this->dataObjectHelper->populateWithArray($parcelShop, $shippingData['parcelShop'], $this->parcelShopClass);
+
+        if ($parcelShop->getNumber()) {
+            $prebooking = $this->api->createPreBooking($parcelShop, $quote, $order);
+            $shippingData['instabox'] = [
+                'prebooking' => $prebooking
+            ];
+            $order->setData('wexo_shipping_data', $this->jsonSerializer->serialize($shippingData));
+            // fetch availability token from session and save token on order
+            $order->getShippingAddress()->addData([
+                OrderAddressInterface::COMPANY => $parcelShop->getCompanyName(),
+                OrderAddressInterface::STREET => [
+                    $parcelShop->getStreetName(),
+                    $parcelShop->getNumber()
+                ],
+                OrderAddressInterface::POSTCODE => $parcelShop->getZipCode(),
+                OrderAddressInterface::CITY => $parcelShop->getCity(),
+                OrderAddressInterface::REGION => '',
+                OrderAddressInterface::FAX => '',
+                'save_in_address_book' => 0,
+            ]);
+        } else {
+            throw new LocalizedException(__('Service Point number was not found!'));
+        }
+    }
+}
